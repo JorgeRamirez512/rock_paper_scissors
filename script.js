c801bf0c02d6ffc6a62c42f47a8505fc25d859f9
JorgeRamirez512 <ramirezjorge364@gmail.com>
2023-12-06 18:18:05 -0600

    Finished playRound function

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
+let playerSelection = prompt("rock, paper, or scissor?").toLowerCase();
+let computerSelection = getComputerChoice();
 
-
-
-// create a function called getCompterChoice that will randomly return either "rock", "paper", or "scissors"
-function getCompterChoice() {
-    // create a variable that is an array that has the choices as items
+function getComputerChoice() {
     let choices = ["rock", "paper", "scissors"];
-    // create the random selector variable
     let randomItem = choices[Math.floor(Math.random() * choices.length)];
-    // return random choice
     return randomItem;
 }
-// print random choice on to the console
-console.log(getCompterChoice())
+
+function playRound(playerSelection, computerSelection) {
+    let win = "You win!";
+    let lose = "You lose.";
+    let tie = "Tie.";
+
+    if (
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "paper" && computerSelection == "rock") ||
+        (playerSelection == "scissors" && computerSelection == "paper")
+    ) {
+        return `${win} ${playerSelection} beats ${computerSelection}`;
+    } else if (playerSelection == computerSelection) {
+        return tie;
+    } else if (
+        (playerSelection == "scissors" && computerSelection == "rock") ||
+        (playerSelection == "rock" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "scissors")
+    ) {
+        return `${lose} Computer selected ${computerSelection}`;
+    } else if (!["rock", "paper", "scissors"].includes(playerSelection)) {
+        return "You selected an invalid choice";
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection));
